d78d73a519dae96e7034acf2c14d195ece833732
Naggayi Daphne <daphnepearl101@gmail.com>
2022-04-25 05:18:28 -0700

    deleting an item

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -14,7 +14,7 @@
       <label> Skills</label>
       <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
       <div v-for="skill in skills" :key="skill" class="pill">
-          {{ skill }}
+        <span @click="deleteSkill(skill)">{{ skill }}</span>
 
       </div>
 
@@ -71,6 +71,12 @@ export default {
                }
               this.tempSkill = ''
            }
+        }, 
+        // deleting a skill 
+        deleteSkill(skill) {
+            this.skills = this.skills.filter((item) => {
+                return skill != item
+            })
         }
     }
 
@@ -108,5 +114,16 @@ export default {
         margin: 0 10px 0 0;
         position: relative;
         top: 2px;
+    }
+    .pill {
+        display: inline-block;
+        margin: 20px 10px 0 0 ;
+        padding: 6px 12px;
+        background: #eee;
+        border-radius: 20px;
+        letter-spacing: 1px;
+        font-weight: bold;
+        color: #777;
+        cursor: pointer;
     }
 </style>
