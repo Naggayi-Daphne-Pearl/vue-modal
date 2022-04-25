ee8e6327a8c50cd62c1bc895054e2c701f39e465
Naggayi Daphne <daphnepearl101@gmail.com>
2022-04-25 03:33:50 -0700

    two way data binding

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,12 +1,33 @@
 <template>
   <form>
       <label> Email</label>
-      <input type="email" required>
+      <input type="email" required v-model="email">
+       <label> Password</label>
+      <input type="password" required v-model="password">
+
+      <label>Role:</label>
+      <select v-model="role">
+          <option value="developer">Web developer</option>
+          <option value="designer">Web designer</option>
+      </select>
   </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+
 </template>
 
 <script>
+// two  way data binding means data is bound in two ways from the input output and data 
 export default {
+    data() {
+        return {
+            email: '', 
+            password: '', 
+            role: ''
+
+        }
+    }
 
 }
 </script>
@@ -29,7 +50,7 @@ export default {
         letter-spacing: 1px;
         font-weight: bold;
     }
-    input {
+    input, select{
         display: block;
         padding: 10px 6px;
         width: 100%;
