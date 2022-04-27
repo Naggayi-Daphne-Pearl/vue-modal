78de5c2466dd176a92699622eeadfedc009bfce7
Naggayi Daphne <daphnepearl101@gmail.com>
2022-04-27 15:29:17 -0700

    router links

---
+++ router/src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium tempora distinctio reiciendis nesciunt quis, vel eius sunt animi voluptate! Ratione quas provident voluptatum cupiditate sed exercitationem doloremque, recusandae impedit fugiat nam laborum maxime voluptatibus. Accusantium unde aliquid magni! Fuga officiis nesciunt eius cumque omnis, accusantium aliquid alias delectus quasi, quis eum fugiat inventore nostrum recusandae provident eveniet. Veniam maiores temporibus ex provident! Delectus adipisci nobis at doloribus optio consequatur, ex laboriosam officiis, in aspernatur provident qui? Non, ipsa eveniet. Nihil quidem magni id maxime eligendi corporis ad officiis autem quaerat? Odio maxime, quos architecto incidunt aut vitae impedit doloribus placeat!</p>
+    <p>lorem50</p>
+  </div>
+
+</template>
---
+++ router/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Home page</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa eligendi asperiores quos porro, temporibus sapiente corporis illum omnis tempore doloribus quis consectetur eos amet fugiat recusandae maiores dicta aspernatur unde? Officiis provident voluptatibus eveniet animi, incidunt cupiditate consequatur perferendis magni amet nisi debitis quas temporibus fugit ducimus dolores doloribus accusantium ea ad, consequuntur error. Iure, sapiente rerum. Eligendi ad harum odit dolore officia possimus recusandae esse, assumenda id aliquid fugiat similique unde eos dolorum laboriosam eius repellat magni blanditiis optio? Praesentium error deserunt, maiores rerum fugiat modi ducimus. Voluptatibus obcaecati ducimus perspiciatis officiis officia ullam tempore sequi vitae nam pariatur!</p>
+    
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non nostrum culpa eligendi, aliquid neque consectetur distinctio consequatur vero iure veniam saepe, praesentium beatae eum nihil perferendis quis minima, expedita dolorum ea dolore id. Mollitia quidem dignissimos fuga assumenda in esse, deserunt laborum eaque cupiditate? Culpa, doloribus beatae. Delectus, ducimus obcaecati?</p>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  components: {}
+}
+</script>
