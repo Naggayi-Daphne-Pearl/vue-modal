5d73622126ab64670c7a70efc0d97da427d8b139
Naggayi Daphne <daphnepearl101@gmail.com>
2022-04-27 17:57:01 -0700

    dynamic links

--- router/src/views/jobs/Jobs.vue
+++ router/src/views/jobs/Jobs.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>JOBS</h1>
   <div v-for="job in jobs" :key ="job.id">
+      <!--Dynamic links-->
       <router-link :to="{name:'JobDetails', params:{ id:job.id}}">
           <h2>{{ job.title }}</h2>
       </router-link>
