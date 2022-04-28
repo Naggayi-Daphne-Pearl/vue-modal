13e76705aba2dd1a1c40b9867ac6fbecb0495dbe
Naggayi Daphne <daphnepearl101@gmail.com>
2022-04-27 18:25:07 -0700

    Programmatic navigation

--- router/src/App.vue
+++ router/src/App.vue
@@ -5,8 +5,28 @@
     <router-link :to="{ name: 'about' }">About</router-link>
     <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </nav>
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go  back</button>
+  <button @click="forward">Go Forward</button>
   <router-view/>
 </template>
+
+<script>
+
+export default {
+    methods: {
+      redirect() {
+        this.$router.push( {name: 'home'})
+      }, 
+      back(){
+        this.$router.go(-1)
+      }, 
+      forward() {
+        this.$router.go(1)
+      }
+    }
+}
+</script>
 
 <style>
 #app {
@@ -33,5 +53,11 @@ nav a {
 nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
