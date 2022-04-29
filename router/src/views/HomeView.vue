e760aa1428ccc41aad0836f3f09e647b7f3127aa
Naggayi Daphne <daphnepearl101@gmail.com>
2022-04-28 17:15:44 -0700

    Template refs

--- router/src/views/HomeView.vue
+++ router/src/views/HomeView.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="home">
     <h1>Home page</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa eligendi asperiores quos porro, temporibus sapiente corporis illum omnis tempore doloribus quis consectetur eos amet fugiat recusandae maiores dicta aspernatur unde? Officiis provident voluptatibus eveniet animi, incidunt cupiditate consequatur perferendis magni amet nisi debitis quas temporibus fugit ducimus dolores doloribus accusantium ea ad, consequuntur error. Iure, sapiente rerum. Eligendi ad harum odit dolore officia possimus recusandae esse, assumenda id aliquid fugiat similique unde eos dolorum laboriosam eius repellat magni blanditiis optio? Praesentium error deserunt, maiores rerum fugiat modi ducimus. Voluptatibus obcaecati ducimus perspiciatis officiis officia ullam tempore sequi vitae nam pariatur!</p>
+    <p ref="p">My name is {{ name }}. I am {{ age }}</p>
+    <button @click="handleClick">Click me</button>
     
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non nostrum culpa eligendi, aliquid neque consectetur distinctio consequatur vero iure veniam saepe, praesentium beatae eum nihil perferendis quis minima, expedita dolorum ea dolore id. Mollitia quidem dignissimos fuga assumenda in esse, deserunt laborum eaque cupiditate? Culpa, doloribus beatae. Delectus, ducimus obcaecati?</p>
 
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 
 export default {
   name: 'home',
-  components: {}
+  setup() {
+    const p = ref(null)
+
+    let name = 'daphne'
+    let age = 24
+    
+    const handleClick = () => {
+      console.log(p, p.value)
+      p.value.textContent ='learning vue'
+    }
+
+    return{name: name, age:age, handleClick, p}
+
+  }, 
+  created() {
+    console.log('created')
+
+  }, 
+  mounted() {
+    console.log('mounted')
+
+  }
 }
 </script>
